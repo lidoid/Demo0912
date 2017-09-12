e4d07845e1d64536bbb6fec9ff98559c2c1e4231
wqdxunxian <1293257853@qq.com>
2017-09-12 16:12:12 +0800

    王琪栋提交

--- app/src/main/java/app/demo0912/MainActivity.java
+++ app/src/main/java/app/demo0912/MainActivity.java
@@ -2,6 +2,7 @@ package app.demo0912;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("asdfg","asdfg");
     }
 }
